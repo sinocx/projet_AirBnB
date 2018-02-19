d997a19daf856a46a8d2eef4e31cc247579258d7
Adam Sarfati <adamsarfat@hotmail.fr>
2018-02-19 18:27:30 +0100

    delete mail/password from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,5 @@ class User < ApplicationRecord
   has_many :products, through: :bookings
   validates :name, presence: true
   validates :address, presence: true
-  validates :mail, uniqueness: true, presence: true
-  validates :password, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219165550) do
+ActiveRecord::Schema.define(version: 20180219172612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,8 +44,6 @@ ActiveRecord::Schema.define(version: 20180219165550) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.string "mail"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
