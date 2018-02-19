0d97b45e3998c4dc6e74abd7c107a6605def9a6a
Adam Sarfati <adamsarfat@hotmail.fr>
2018-02-19 18:06:35 +0100

    on reprend sur de bonne base

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :bookings
   has_many :products, through: :bookings
   validates :name, presence: true
   validates :address, presence: true
-  validates :mail, uniqueness: true, presence: true, format: { with: /\A.*@.*\.com\z/ }
+  validates :mail, uniqueness: true, presence: true
   validates :password, presence: true
 end
