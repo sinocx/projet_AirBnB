d8d97d913af6a8d7f350ce685130140a383c6df2
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-02-19 17:43:51 +0100

    i have change the product, create _forme, create the create methode and add the view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,11 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to cocktail_path(@product)
+    else render :new
+    end
   end
 
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,10 @@
 class Product < ApplicationRecord
+  CATEGORY = [ "skateboard", "vélo", "trottinette" ]
+
   belongs_to :user
   has_many :bookings
-  validates :tite, presence: true
-  validates :category, presence: true
+  validates :title, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORY }
   validates :description, presence: true
   validates :price, presence: true
   validates :rent, default: :false
