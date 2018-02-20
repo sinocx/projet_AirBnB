7c222899861a6a5788ceb726ea96f65bbb61212b
David Crenin <david.crenin@gmail.com>
2018-02-20 11:22:17 +0100

    Install Pundit - Controller Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,21 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = policy_scope(Product).order(created_at: :desc)
   end
 
   def show
     @product = Product.find(params[:id])
+    authorize @product
   end
 
   def new
+    @product = Product.new
+    authorize @product
   end
 
   def create
     @product = Product.new(product_params)
+    authorize @product
     if @product.save
       redirect_to product_path(@product)
     else render :new
@@ -19,11 +23,15 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    @product = Product.find(params[:id])
+    authorize @product
+    @product.destroy
+    redirect_to product_path
   end
 
   private
   def product_params
-    params.require(:product).permit(:title, :category, :user_id, :price, :image, :address, :rent)
+    params.require(:product).permit(:title, :category, :price, :image, :address, :rent)
   end
 
 end
