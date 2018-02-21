f567df913a0cabb5dca5778f76ed5816af54f5b6
Adam Sarfati <adamsarfat@hotmail.fr>
2018-02-21 16:14:40 +0100

    debug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,9 +36,10 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     @product.user = current_user
     authorize @product
-    if @product.save!
+    if @product.save
       redirect_to product_path(@product)
-    else render :new
+    else
+      render :new
     end
   end
 
