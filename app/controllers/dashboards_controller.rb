360a8d40f99e9e61333a7ac7318b2bb7926edc00
Adam Sarfati <adamsarfat@hotmail.fr>
2018-02-23 16:13:20 +0100

    dash git add .

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,11 +3,10 @@ class DashboardsController < ApplicationController
 
   def dashboard
     @bookings = current_user.bookings
-
     @product_bookings = current_user.products.map{ |product| product.bookings.where(status: ["Pending", "Validate", "Refuse"]) }.flatten
-     @product_bookings.sort_by! do |b|
-        b.status
-      end
+    @product_bookings.sort_by! do |b|
+      b.status
+    end
     # @product_bookings = current_user.bookings
 
     @products = current_user.products
